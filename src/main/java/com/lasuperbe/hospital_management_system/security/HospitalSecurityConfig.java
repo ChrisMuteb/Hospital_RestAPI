04533651a613d18dea9941b50536e9f778fbdada
ChrisMuteb <ibond.chris@gmail.com>
2023-08-12 12:36:15 +0200

    Basic security added to the project.

---
+++ src/main/java/com/lasuperbe/hospital_management_system/security/HospitalSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.lasuperbe.hospital_management_system.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class HospitalSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE" ,"MANAGER").build();
+        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/doctors").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/doctors/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/doctors").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/doctors").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/doctors/**").hasRole("ADMIN")
+        );
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
